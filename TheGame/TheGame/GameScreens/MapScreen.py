68e8bb2d4cc01f792e072bd194e8557243e39fc5
Emile Taverne <emile_taverne@hotmail.com>
2015-02-05 23:18:55 +0100

    Modification dialog

--- TheGame/TheGame/GameScreens/MapScreen.py
+++ TheGame/TheGame/GameScreens/MapScreen.py
@@ -7,6 +7,11 @@ from kivy.uix.widget import Widget, Builder
 from kivy.uix.layout import Layout
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
+from kivy.animation import Animation
+from kivy.graphics.transformation import Matrix
+from kivy.clock import Clock
+
+from functools import partial
 
 from GameScreen import GameScreen
 from Dialog.DialogWidget import DialogWidget
@@ -20,22 +25,21 @@ class MapScreen(GameScreen):
     def click(self):
         mouse = self.app.app.window.mouse_pos
         width = self.size[0]
-        heigth = self.size[1]
+        height = self.size[1]
 
-        if(mouse[0] > 0.20 * width and mouse[0] < 0.75 * width and mouse[1] < 0.95 * heigth and mouse[1] > 0.138 * heigth):
-            if(mouse[0] < 0.44375 * width and mouse[1] > 0.566 * heigth):
+        if(mouse[0] > 0.20 * width and mouse[0] < 0.75 * width and mouse[1] < 0.95 * height and mouse[1] > 0.138 * height):
+            if(mouse[0] < 0.44375 * width and mouse[1] > 0.566 * height):
                 """ Case MOE """
-                print " Case MOE "
-            elif(mouse[0] > 0.44375 * width and mouse[1] > 0.75 * heigth):
+            elif(mouse[0] > 0.44375 * width and mouse[1] > 0.75 * height):
                 """ Case RH """
                 print " Case RH "
-            elif(mouse[0] < 0.34375 * width and mouse[1] < 0.394 * heigth):
+            elif(mouse[0] < 0.34375 * width and mouse[1] < 0.394 * height):
                 """ Case bureau """
                 print " Case bureau "
-            elif(mouse[0] > 0.34375 * width and mouse[0] < 0.56875 * width and mouse[1] < 0.394 * heigth):
+            elif(mouse[0] > 0.34375 * width and mouse[0] < 0.56875 * width and mouse[1] < 0.394 * height):
                 """ Case marketing """
                 print " Case marketing "
-            elif(mouse[0] > 0.56875 * width and mouse[1] < 0.577 * heigth):
+            elif(mouse[0] > 0.56875 * width and mouse[1] < 0.577 * height):
                 """ Case patron """
                 print " Case patron "
             else:
@@ -44,7 +48,7 @@ class MapScreen(GameScreen):
         else:
             """ En dehors de la map """
             print " En dehors de la map "
-        
+
 
 class IconButton(ButtonBehavior, Image):
     pass
