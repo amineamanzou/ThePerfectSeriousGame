2c86da1d173709b554191f2cb14033152d8d807c
djbz <djbz@free.fr>
2015-02-16 01:02:14 +0100

    add information question (excalmation)

--- TheGame/TheGame/GameScreens/MapScreen.py
+++ TheGame/TheGame/GameScreens/MapScreen.py
@@ -7,11 +7,14 @@ kivy.require('1.8.0')
 from kivy.uix.widget import Widget, Builder
 from kivy.uix.layout import Layout
 from kivy.animation import Animation
+from kivy.uix.image import Image
 
 from GameScreen import GameScreen
 from Dialog.DialogWidget import DialogWidget
 from CustomWidget.DynImage import DynImage
 from CustomWidget.ScoreSlider import ScoreSlider
+
+import os
 
 Builder.load_file("GameScreens/MapScreen.kv")
 
@@ -33,6 +36,11 @@ class MapScreen(GameScreen):
                 slider = ScoreSlider(size=("100dp", "30dp"))
                 slider.pos = (room.xMax * self.size[0] - slider.size[0], room.yMin * self.size[1])
                 self.ids.map.add_widget(slider)
+            if(self.app.gameManager.dialogAvailableInRoom(room.id)):
+                warning = Image(source=(self.app.app.APPLICATION_PATH + os.path.normpath('/Images/warning.png')), size=("80dp", "80dp"))
+                warning.pos = (room.xMax * self.size[0] - warning.size[0], room.yMin * self.size[1])
+                self.ids.map.add_widget(warning)
+
        
     def click(self):
         if not self.ids.dialog.visible:
@@ -44,7 +52,7 @@ class MapScreen(GameScreen):
                 if(mouse[0] > room.xMin * width and mouse[0] < room.xMax * width and mouse[1] > room.yMin * height and mouse[1] < room.yMax * height):
                     if(room.name == "bureau"):
                         self.app.changeScreen("DeskScreen")
-                    elif(room.name == "boss"):
+                    elif(room.name == "boss" and self.app.gameManager.end):
                         self.app.changeScreen("BossScreen")
                     else:
                         try:
